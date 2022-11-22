988cd08c243e57e6c3ff38c323c52b6eeb775c2e
VisheshBajpayee <vishesh.bajpayee@360technosoft.com>
2022-11-22 12:56:47 +0530

    added changes to navbar

--- utility-app/src/components/Navbar.tsx
+++ utility-app/src/components/Navbar.tsx
@@ -13,7 +13,16 @@ const Navbar = (props: Props) => {
         <button className="btn btn-ghost">notes</button>
         <button className="btn btn-ghost">pomodoro</button>
       </div>
-
+      <div>
+        specifics components are provided for the controlled components are
+        hence there are so many Things in this world on which we dont have any
+        control loke earthquakes tsunamies and what not but the thing is that on
+        what we can like to forgive someone or to spread hapiness without
+        expecting any in return
+      </div>
+      <div>
+        <h1 className="specifics">Specifics !</h1>
+      </div>
       <div className="navbar-end">
         <CiSettings className="cursor-pointer" size={30} color="white" />
       </div>
